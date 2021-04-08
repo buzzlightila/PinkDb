c0badc478bb5c0c2f9c710e2cd986930aebd55ab
buzzlightila <superatilarangel@gmail.com>
2021-04-08 20:39:05 -0300

    Remove colors and add new gif

--- main.c
+++ main.c
@@ -4,8 +4,6 @@
 #include "cmd/struct/Table.h"
 #include "cmd/create/main.h"
 #include "cmd/show/main.h"
-#include "utils/color.h"
-
 
 typedef enum {true, false} bool;
 
@@ -13,18 +11,14 @@ int main () {
   bool quit = true;
   char cmd[10], params[10], name[10] = "";
   while(quit == true) {
-    // choseColor("");
     printf("au> ");
-    // choseColor("reset");
     scanf("%s %s %s", cmd, params, name);
 
-    // choseColor("");
     if (strcmp(cmd, "q") == 0 || strcmp(cmd, "quit") == 0) quit = false;
     else if (strcmp(cmd, "help") == 0) help();
     else if (strcmp(cmd, "create") == 0) create(params, name);
     else if (strcmp(cmd, "show") == 0) show(params, name);
     else printf("auauau: Unknow '%s' command. Use help\n", cmd);
-    // choseColor("reset");
   }
   return 0;
 }
